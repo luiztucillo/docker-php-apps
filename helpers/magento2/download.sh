c89028e2ea810bfaaf002c6da5372ff465829b47
“Luiz Tucillo” <luiz.tucillo@mercadolivre.com>
2019-12-17 17:55:45 -0300

    IMprove composer

--- helpers/magento2/download.sh
+++ helpers/magento2/download.sh
@@ -4,7 +4,17 @@
 DIR=$(dirname $0)
 APPLICATION=$(grep APPLICATION ${DIR}/../../.env | cut -d '=' -f2)
 
-docker container exec -it ${APPLICATION}_php composer global require -vvv hirak/prestissimo \
-  && docker container exec -it --workdir /var/www/html ${APPLICATION}_php \
-       composer create-project -vvv --repository-url=https://repo.magento.com/ magento/project-community-edition /var/www/html \
-  || docker container exec -it --workdir /var/www/html ${APPLICATION}_php composer update -vvv
+docker container exec --workdir /var/www/html ${APPLICATION}_php \
+       composer create-project -vvv --repository-url=https://repo.magento.com/ magento/project-community-edition /var/www/html/tmp
+
+docker container exec --workdir /var/www/html/tmp ${APPLICATION}_php \
+       composer update -vvv
+
+docker container exec --workdir /var/www/html ${APPLICATION}_php \
+       mv /var/www/html/tmp/* /var/www/html
+
+docker container exec --workdir /var/www/html ${APPLICATION}_php \
+       mv /var/www/html/tmp/.* /var/www/html \
+
+docker container exec --workdir /var/www/html ${APPLICATION}_php \
+       rm -rf /var/www/html/tmp
