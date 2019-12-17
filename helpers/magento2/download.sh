6d77ee3e2e9ec972b37d273ecccebaf79e6c34ef
“Luiz Tucillo” <luiz.tucillo@mercadolivre.com>
2019-12-17 18:33:59 -0300

    Fix magento2 downloader helper

--- helpers/magento2/download.sh
+++ helpers/magento2/download.sh
@@ -7,14 +7,11 @@ APPLICATION=$(grep APPLICATION ${DIR}/../../.env | cut -d '=' -f2)
 docker container exec --workdir /var/www/html ${APPLICATION}_php \
        composer create-project -vvv --repository-url=https://repo.magento.com/ magento/project-community-edition /var/www/html/tmp
 
-docker container exec --workdir /var/www/html/tmp ${APPLICATION}_php \
-       composer update -vvv
+docker container exec --workdir /var/www/html ${APPLICATION}_php \
+       mv tmp/* ./
 
 docker container exec --workdir /var/www/html ${APPLICATION}_php \
-       mv /var/www/html/tmp/* /var/www/html
-
-docker container exec --workdir /var/www/html ${APPLICATION}_php \
-       mv /var/www/html/tmp/.* /var/www/html \
+       mv tmp/.* ./
 
 docker container exec --workdir /var/www/html ${APPLICATION}_php \
        rm -rf /var/www/html/tmp
